025f0c5fe20c99a6392c9bd4afdbccf4133a1d5e
Luis Salinas <luigicoder28@gmail.com>
2021-07-15 10:16:25 -0300

    Calculadora JS

---
+++ Calculadora.js
@@ -0,0 +1,15 @@
+class Calculadora {
+
+    suma(num1, num2){
+        return num1 + num2;
+    }
+    restar(num1, num2){
+        return num1 - num2;
+    }
+    dividir(num1, num2){
+        return num1 / num2;
+    }
+    multiplicar(num1, num2){
+        return num1 * num2;
+    }
+}
